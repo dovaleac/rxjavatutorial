54de49c637607676d880e03a31e3caaa6823fbf9
Pablo González Doval <pablo.gonzalez@masmovil.com>
2019-05-30 18:13:35 +0200

    Added random time waiting

--- src/main/java/com/dovaleac/rxjava2tutorial/services/ReadService.java
+++ src/main/java/com/dovaleac/rxjava2tutorial/services/ReadService.java
@@ -2,6 +2,8 @@ package com.dovaleac.rxjava2tutorial.services;
 
 import com.dovaleac.rxjava2tutorial.domain.Character;
 import com.dovaleac.rxjava2tutorial.domain.House;
+import com.dovaleac.rxjava2tutorial.util.TimeWaiter;
+import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.Maybe;
 import io.reactivex.Single;
@@ -12,6 +14,7 @@ import java.util.Random;
 public class ReadService {
   private final List<Character> characters;
   private final List<House> houses;
+  private final TimeWaiter waiter = TimeWaiter.READ_WAITER;
 
   public ReadService(List<Character> characters,
       List<House> houses) {
@@ -20,19 +23,33 @@ public class ReadService {
   }
 
   public Flowable<Character> getAllCharacters() {
-    return Flowable.fromIterable(characters);
+    return Flowable.create(flowableEmitter -> {
+      waiter.waitRandom();
+      characters.forEach(flowableEmitter::onNext);
+      flowableEmitter.onComplete();
+    }, BackpressureStrategy.BUFFER);
   }
 
   public Flowable<House> getAllHouses() {
-    return Flowable.fromIterable(houses);
+    return Flowable.create(flowableEmitter -> {
+      waiter.waitRandom();
+      houses.forEach(flowableEmitter::onNext);
+      flowableEmitter.onComplete();
+    }, BackpressureStrategy.BUFFER);
   }
 
   public Single<Character> getRandomCharacter() {
-    return Single.just(characters.get(new Random().nextInt(characters.size()-1)));
+    return Single.create(singleEmitter -> {
+      waiter.waitRandom();
+      singleEmitter.onSuccess(characters.get(new Random().nextInt(characters.size() - 1)));
+    });
   }
 
   public Single<House> getRandomHouse() {
-    return Single.just(houses.get(new Random().nextInt(houses.size()-1)));
+    return Single.create(singleEmitter -> {
+      waiter.waitRandom();
+      singleEmitter.onSuccess(houses.get(new Random().nextInt(houses.size()-1)));
+    });
   }
 
   public Maybe<Character> getCharacterById(int id) {
--- src/main/java/com/dovaleac/rxjava2tutorial/services/WriteService.java
+++ src/main/java/com/dovaleac/rxjava2tutorial/services/WriteService.java
@@ -3,6 +3,7 @@ package com.dovaleac.rxjava2tutorial.services;
 import com.dovaleac.rxjava2tutorial.domain.Character;
 import com.dovaleac.rxjava2tutorial.domain.House;
 import com.dovaleac.rxjava2tutorial.exceptions.InexistentHouseException;
+import com.dovaleac.rxjava2tutorial.util.TimeWaiter;
 import io.reactivex.Completable;
 import io.reactivex.Flowable;
 import io.reactivex.Maybe;
@@ -14,6 +15,7 @@ import java.util.Random;
 public class WriteService {
   private final List<Character> characters;
   private final List<House> houses;
+  private final TimeWaiter waiter = TimeWaiter.WRITE_WAITER;
 
   public WriteService(List<Character> characters,
       List<House> houses) {
@@ -22,15 +24,22 @@ public class WriteService {
   }
 
   public Completable addNewHouse(House house) {
-    return Completable.fromAction(() -> houses.add(house));
+    return Completable.fromAction(() -> {
+      waiter.waitRandom();
+      houses.add(house);
+    });
   }
 
   public Completable addNewCharacter(Character character) {
-    return Completable.fromAction(() -> characters.add(character));
+    return Completable.fromAction(() -> {
+      waiter.waitRandom();
+      characters.add(character);
+    });
   }
 
   public Completable changeHouseRuler(House house, Character newRuler) {
     return Completable.fromAction(() -> {
+      waiter.waitRandom();
       if (!houses.remove(house)) {
         throw new InexistentHouseException(house);
       }
