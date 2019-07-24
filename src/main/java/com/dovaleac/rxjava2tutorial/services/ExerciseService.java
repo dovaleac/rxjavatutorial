ccd540925e046b9dd63241e4b3f0981673f18a01
Pablo González Doval <pablo.gonzalez@masmovil.com>
2019-07-24 09:02:16 +0200

    Added javadoc

--- src/main/java/com/dovaleac/rxjava2tutorial/services/ExerciseService.java
+++ src/main/java/com/dovaleac/rxjava2tutorial/services/ExerciseService.java
@@ -16,21 +16,42 @@ import java.util.stream.Collectors;
 
 public class ExerciseService {
 
+  /**
+   * Obtain the names of all the characters
+   * @param status The way to access everything
+   * @return
+   */
   public Flowable<String> getAllCharactersNames(Status status) {
     return status.getReadService().getAllCharacters().map(Character::getName);
   }
 
+  /**
+   * Obtain the names of all the characters and sort them by the number of letters they have
+   * @param status The way to access everything
+   * @return
+   */
   public Flowable<String> getAllCharactersNamesSortedByNumberOfLetters(Status status) {
     return status.getReadService().getAllCharacters().map(Character::getName)
         .sorted(Comparator.comparingInt(String::length));
   }
 
+  /**
+   * Obtain all the characters who possess at least one non-empty title.
+   * Note: some characters have only one title and it's empty. Those shouldn't be shown.
+   * @param status The way to access everything
+   * @return
+   */
   public Flowable<Character> getAllCharactersWithTitles(Status status) {
     return status.getReadService().getAllCharacters()
         .filter(character -> !character.getTitles().isEmpty()
             && !(character.getTitles().size() == 1 && character.getTitles().get(0).isEmpty()));
   }
 
+  /**
+   * Obtain the character who has the most titles.
+   * @param status The way to access everything
+   * @return
+   */
   public Maybe<Character> getCharacterWithMostTitles(Status status) {
     return status.getReadService().getAllCharacters()
         .reduce((character, character2) -> {
@@ -44,16 +65,32 @@ public class ExerciseService {
         });
   }
 
+  /**
+   * The same as the previous exercise, but returning a Single, as we know at least 1 character
+   * exists
+   * @param status The way to access everything
+   * @return
+   */
   public Single<Character> getCharacterWithMostTitlesAsSingle(Status status) {
     return getCharacterWithMostTitles(status)
         .toSingle();
   }
 
+  /**
+   * Obtain a map that, for each house, obtains the length of its motto (the house's words)
+   * @param status The way to access everything
+   * @return
+   */
   public Single<Map<String, Integer>> getHousesWithMottoLength(Status status) {
     return status.getReadService().getAllHouses()
         .toMap(House::getName, house -> house.getWords().length());
   }
 
+  /**
+   * Obtain all the characters who are lords of anything in the "Dorne" region
+   * @param status The way to access everything
+   * @return
+   */
   public Flowable<Character> getAllDornishLords(Status status) {
     return status.getReadService().getAllHouses()
         .filter(house -> Objects.equals(house.getRegion(), "Dorne"))
@@ -61,12 +98,26 @@ public class ExerciseService {
         .flatMapMaybe(id -> status.getReadService().getCharacterById(id));
   }
 
+  /**
+   * Get all the houses whose overlord house's overlord house is the parameterized one
+   * Note: please note the method getOverlordedByHouse in ReadService
+   * @param status The way to access everything
+   * @param house The house to search for
+   * @return
+   */
   public Flowable<House> getOverlordedsOverlorded(Status status, House house) {
     return status.getReadService().getOverlordedByHouse(house)
         .flatMap(house1 -> status.getReadService().getOverlordedByHouse(house1))
         .distinct();
   }
 
+  /**
+   * Create a map that for each dornish lord (remember we have an exercise in which you had to
+   * calculate all the dornish lords) specifies which % of all the titles among dornish lords
+   * s/he possesses. E.g: if there are only 2 noblemen and both have 2 titles, both would have 50%
+   * @param status The way to access everything
+   * @return
+   */
   public Single<Map<String, Double>> getDornishLordsShareOfTitles(Status status) {
 
     return getAllDornishLords(status)
@@ -82,11 +133,26 @@ public class ExerciseService {
         });
   }
 
+  /**
+   * In this exercise you have to add a new house, and then return that house's overlordeds'
+   * overlordeds (remember we have an exercise for this last part)
+   * @param status The way to access everything
+   * @param house The house to add
+   * @return
+   */
   public Flowable<House> getOverlordedOfNewHousesOverlorded(Status status, House house) {
     return status.getWriteService().addNewHouse(house)
         .andThen(getOverlordedsOverlorded(status, house));
   }
 
+  /**
+   * Add a new house, add a new character, set the character as the ruler of the house and then
+   * check the house's ruler
+   * @param status The way to access everything
+   * @param newHouse The house to add
+   * @param newRuler The house's new ruler
+   * @return
+   */
   public Single<Character> addHouseAndAddRulerAndCheckItsRuler(Status status, House newHouse,
       Character newRuler) {
     return Completable.concatArray(
@@ -97,15 +163,6 @@ public class ExerciseService {
         .map(House::getCurrentLord)
         .flatMap(id -> status.getReadService().getCharacterById(id))
         .toSingle();
-  }
-
-  /*
-  * recursively get overlorded
-  * */
-  public Flowable<House> getAllOverlordedByHouse(Status status, House overlord) {
-    return status.getReadService()
-        .getOverlordedByHouse(overlord)
-        .flatMap()
   }
 
 }
